c26fb9f9be58e10a992d979272f85e15aea5fc80
Scott Caruso <scottcaruso@fullsail.edu>
2014-03-17 00:32:43 -0500

    Created class for leaderboards/sign-in management

---
+++ MGD1402/MGD1402/LeaderboardsAndSignIn.h
@@ -0,0 +1,13 @@
+//
+//  LeaderboardsAndSignIn.h
+//  MGD1402
+//
+//  Created by Scott Caruso on 3/17/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LeaderboardsAndSignIn : NSObject
+
+@end
---
+++ MGD1402/MGD1402/LeaderboardsAndSignIn.m
@@ -0,0 +1,43 @@
+//
+//  LeaderboardsAndSignIn.m
+//  MGD1402
+//
+//  Created by Scott Caruso on 3/17/14.
+//  Copyright (c) 2014 Scott Caruso. All rights reserved.
+//
+
+#import "LeaderboardsAndSignIn.h"
+
+@implementation LeaderboardsAndSignIn
+{
+    
+}
+
+-(id)init
+{
+    return self;
+}
+
+-(void)checkIfUserNameExists
+{
+    
+}
+
+-(void)checkIfPasswordIsCorrect
+{
+    
+}
+
+-(void)saveUsersDataLocally
+{
+    
+}
+
+-(void)createNewAccount
+{
+    
+}
+
+
+
+@end
