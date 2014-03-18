29fbb40ac6e1557ec2b4b8b2a4f401d1f1f94c25
Scott Caruso <scottcaruso@fullsail.edu>
2014-03-17 23:14:24 -0500

    Created three base functions - username/password verification, new user creation, and high score update. These still need some refinement, but core pieces are there.

--- MGD1402/MGD1402/LeaderboardsAndSignIn.m
+++ MGD1402/MGD1402/LeaderboardsAndSignIn.m
@@ -18,24 +18,57 @@
     return self;
 }
 
--(void)checkIfUserNameExists
+-(void)logUserIn:(NSString*)enteredName password:(NSString*)enteredPassword
 {
-    
+    PFQuery *query = [PFQuery queryWithClassName:@"Leaderboards"];
+    [query whereKey:@"playerName" equalTo:enteredName];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            // The find succeeded.
+            // Do something with the found objects
+            for (PFObject *object in objects) {
+                NSString *thisPassword = [object objectForKey:@"password"];
+                if ([thisPassword isEqualToString:enteredPassword])
+                {
+                    //Success!
+                } else
+                {
+                    //Failure!
+                }
+            }
+        } else {
+            //Throw up the Username not found error.
+        }
+    }];
 }
 
--(void)checkIfPasswordIsCorrect
+//This will take the new high score, the user's efficiency rating for that high score, and the ParseID and push the new score to the cloud.
+-(void)pushNewScoreToLeaderboard:(NSString*)highScore userID:(NSString*)parseID efficiency:(NSString*)efficiency
 {
+    PFQuery *query = [PFQuery queryWithClassName:@"Leaderboards"];
     
+    // Retrieve the object by id
+    [query getObjectInBackgroundWithId:parseID block:^(PFObject *thisUser, NSError *error) {
+        
+        thisUser[@"high_score"] = highScore;
+        thisUser[@"efficiency"] = efficiency;
+        [thisUser saveInBackground];
+    }];
 }
 
--(void)saveUsersDataLocally
+-(void)createNewAccount:(NSString*)enteredName password:(NSString*)enteredPassword
 {
-    
-}
-
--(void)createNewAccount
-{
-    
+    PFQuery *query = [PFQuery queryWithClassName:@"Leaderboards"];
+    [query whereKey:@"playerName" equalTo:enteredName];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error)
+        {
+            // The username already exists! Throw up an appropriate alert message.
+        } else
+        {
+            // Create the user account with the specified credentials.
+        }
+    }];
 }
 
 
